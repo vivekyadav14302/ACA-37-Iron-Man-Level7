db8d981cbe257b4de395bd4f8c293965876ac69a
Vivek Yadav <fastvivekyadav@gmail.com>
2021-09-06 13:42:47 +0530

    Iron Man Level 5

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-
+// load variables
 var bg, backgroundImg;
 
 function preload() {
@@ -11,6 +11,7 @@ function preload() {
 }
 
 function setup() {
+  // create Character , groups and other settings
   createCanvas(1000, 600);
   bg = createSprite(580,300);
   bg1 = createSprite(580,-300);
@@ -22,14 +23,15 @@ function setup() {
   bg1.velocityY = 3;
   ironMan = createSprite(100,450,150,40);
   ironMan.addImage(ironManImg);
- ironMan.scale = 0.3
- score = 0
+  ironMan.scale = 0.3
+  score = 0
   stoneGroup = new Group();
   diamondGroup = new Group();
   spikesGroup = new Group();
 }
 
 function draw() {
+  // game control keys
   if(keyDown("up")){
     ironMan.velocityY = -10;
   }
@@ -41,8 +43,9 @@ function draw() {
   if(keyDown("right")){
     ironMan.x = ironMan.x + 5;
   }
-    ironMan.velocityY = ironMan.velocityY + 0.5;
 
+    ironMan.velocityY = ironMan.velocityY + 0.5;
+  // bg control
     if(bg.y > 900){
       bg.y = -300
     }
@@ -50,21 +53,23 @@ function draw() {
     if(bg1.y > 900){
       bg1.y = -300
     } 
+    // stone come in seconds
      if(frameCount%90 == 0){
       stonegen()
   }
-
+  // ironMan touching settings to stone 
     for(k = 0; k < (stoneGroup).length; k++){
-      mystone = stoneGroup.get(k)
+        mystone = stoneGroup.get(k)
       if(ironMan.isTouching(mystone)){
           ironMan.collide(mystone)
       }
   }
 
-
+// diamond come in seconds
   if(frameCount%130 == 0){
-    diamondgen()
+     diamondgen()
 }
+// ironMan touching settings to diamonds
 for(u = 0; u < (diamondGroup).length; u++){
     mydiamond = diamondGroup.get(u)
     if(ironMan.isTouching(mydiamond)){
@@ -73,10 +78,11 @@ for(u = 0; u < (diamondGroup).length; u++){
         score++
     }
 }
-
+// spikes come in seconds
   if(frameCount%120 == 0){
     spikesgen()
 }
+// ironMan touching settings to spikes
 for(w = 0; w < (spikesGroup).length; w++){
     myspikes = spikesGroup.get(w)
     if(ironMan.isTouching(myspikes)){
@@ -86,12 +92,13 @@ for(w = 0; w < (spikesGroup).length; w++){
 }
 
     drawSprites();
+    // score genrator code
     textSize(24)
     fill("white")
     text("Diamonds Collected: " + score, 50, 50)
    
 }
-
+// stones create function
 function stonegen(){
   s = createSprite(300,10,200,60);
   s.addImage(stoneimg)
@@ -101,7 +108,7 @@ function stonegen(){
   stoneGroup.add(s)
 
 }
-
+// diamonds create function
 function diamondgen(){
   d = createSprite(300,10,200,60);
   d.addImage(diamondImg)
@@ -111,7 +118,7 @@ function diamondgen(){
   d.scale = 0.7
   diamondGroup.add(d)
 }
-
+// spikes create function
 function spikesgen(){
   p = createSprite(300,10,200,60);
   p.addImage(spikesImg)
